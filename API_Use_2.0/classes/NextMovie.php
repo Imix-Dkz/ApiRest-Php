fd0366276ff399341a2f1c1470d2a15f2fabcf90
Imix-Dkz <imix.icm@gmail.com>
2024-04-23 18:15:14 -0600

    Se mueve la función render template, para no depender de functions.php

--- API_Use_2.0/classes/NextMovie.php
+++ API_Use_2.0/classes/NextMovie.php
@@ -12,37 +12,43 @@
     ){}
 
     //Metodos de la clase
-    public function get_until_message():string{
-        $days = $this->days_until;
+        public function get_until_message():string{
+            $days = $this->days_until;
 
-        return match(true){
-            $days == 0   => "¡Hoy se estrena! 🥳",
-            $days == 1   => "Mañana se estrena 😜",
-            $days < 7    => "Se estrena está semana 😦",
-            $days < 30   => "Estamos en el mes de estreno 📅",
-            default     => "$days días hasta el estreno",
-        };        
-    }
-    public function get_data(){
-        return get_object_vars($this);
-    }
+            return match(true){
+                $days == 0   => "¡Hoy se estrena! 🥳",
+                $days == 1   => "Mañana se estrena 😜",
+                $days < 7    => "Se estrena está semana 😦",
+                $days < 30   => "Estamos en el mes de estreno 📅",
+                default     => "$days días hasta el estreno",
+            };        
+        }
+        public function get_data(){
+            return get_object_vars($this);
+        }
 
-    //Static Functións, NO CREQUIEREN contructor
-    public static function fetch_and_create_movie(string $api_url):NextMovie{
-        $result = file_get_contents($api_url);
-        $data = json_decode($result, true);
+    //STATIC Functións, NO CREQUIEREN contructor
+        public static function fetch_and_create_movie(string $api_url):NextMovie{
+            $result = file_get_contents($api_url);
+            $data = json_decode($result, true);
 
-        return new self(
-            $data['days_until'],
-            $data['title'],
-            $data['following_production'] ?? 'Desconocido',
-            $data['release_date'],
-            $data['poster_url'],
-            $data['overview'],
-        );
+            return new self(
+                $data['days_until'],
+                $data['title'],
+                $data['following_production'] ?? 'Desconocido',
+                $data['release_date'],
+                $data['poster_url'],
+                $data['overview'],
+            );
 
-        //return $data;
-    }
+            //return $data;
+        }
+        //Para no depender de la "funciones.php"
+        public static function render_template(string $template, array $data=[])
+        { //Aqui se realizará una validación de las plantillas a renderizar
+            extract($data);
+            require_once "templates/$template.php";
+        }
     
  }
 
--- API_Use_2.0/index.php
+++ API_Use_2.0/index.php
@@ -1,17 +1,16 @@
 <?php
     require_once 'consts.php';
-    require_once 'functions.php'; //<- Funciones estrictas AQUI
     require_once 'classes/NextMovie.php';
 
     $next_movie = NextMovie::fetch_and_create_movie(API_URL);
     $next_movie_data = $next_movie->get_data();
     
     //Sección de plantillas
-    render_template('head', ['title'=>$next_movie_data['title']]); //<= Aqui como sólo se ocupa el title
+    NextMovie::render_template('head', ['title'=>$next_movie_data['title']]); //<= Aqui como sólo se ocupa el title
     //Antes de mandar el $next_movie_data, hay que mandar tambien el $untilMessage
-    render_template('main', array_merge( $next_movie_data,
+    NextMovie::render_template('main', array_merge( $next_movie_data,
         ['until_message' => $next_movie->get_until_message()]
     ));
-    render_template('styles');
+    NextMovie::render_template('styles');
 ?>
 </html>
